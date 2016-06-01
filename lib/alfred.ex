b53714eaef4c3f37a945fbdd02dc33f8ca13ca43
mtanzi <tanzi.marco@gmail.com>
2016-06-01 01:00:40 +0100

    refactor application

--- lib/alfred.ex
+++ lib/alfred.ex
@@ -1,19 +1,7 @@
 defmodule Alfred do
   use Application
 
-  # See http://elixir-lang.org/docs/stable/elixir/Application.html
-  # for more information on OTP Applications
   def start(_type, _args) do
-    import Supervisor.Spec, warn: false
-
-    children = [
-      # Define workers and child supervisors to be supervised
-      worker(Alfred.Slack, []),
-    ]
-
-    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
-    # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: Alfred.Supervisor]
-    Supervisor.start_link(children, opts)
+    Alfred.Supervisor.start_link
   end
 end
